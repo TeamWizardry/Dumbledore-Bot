a9baf8e17d9cde7890963d2a74b33dfa2f584d1d
Demoniaque <saadodi44@gmail.com>
2018-08-12 21:20:31 +0300

    remove me check

--- src/main/java/com/teamwizardry/wizardrybot/module/ModulePlay.java
+++ src/main/java/com/teamwizardry/wizardrybot/module/ModulePlay.java
@@ -59,135 +59,133 @@ public class ModulePlay extends Module implements ICommandModule {
 
 	@Override
 	public void onCommand(DiscordApi api, Message message, Command command, Result result) {
-		if ((String.valueOf(message.getAuthor().getId())).equals("136826665069314048")) {
-			Thread thread = new Thread(() -> {
-				if (command.hasSaidHey() || message.getContent().toLowerCase(Locale.getDefault()).contains("alexa")) {
+		Thread thread = new Thread(() -> {
+			if (command.hasSaidHey() || message.getContent().toLowerCase(Locale.getDefault()).contains("alexa")) {
 
-					String songName = result.getStringParameter("any");
-					if (songName == null || songName.isEmpty()) return;
+				String songName = result.getStringParameter("any");
+				if (songName == null || songName.isEmpty()) return;
 
-					String videoId = null;
-					String title = null;
-					try {
-						HttpResponse<JsonNode> response = Unirest.get("https://www.googleapis.com/youtube/v3/search?q=" + URLEncoder.encode(songName, "UTF-8") +
-								"&maxResults=5" +
-								"&part=snippet" +
-								"&key=" + Keys.YOUTUBE)
-								.asJson();
+				String videoId = null;
+				String title = null;
+				try {
+					HttpResponse<JsonNode> response = Unirest.get("https://www.googleapis.com/youtube/v3/search?q=" + URLEncoder.encode(songName, "UTF-8") +
+							"&maxResults=5" +
+							"&part=snippet" +
+							"&key=" + Keys.YOUTUBE)
+							.asJson();
 
-						JsonElement element = new JsonParser().parse(response.getBody().toString());
-						if (!element.isJsonObject()) return;
-						JsonObject object = element.getAsJsonObject();
+					JsonElement element = new JsonParser().parse(response.getBody().toString());
+					if (!element.isJsonObject()) return;
+					JsonObject object = element.getAsJsonObject();
 
-						if (object.has("items") && object.get("items").isJsonArray()) {
-							for (JsonElement itemElement : object.getAsJsonArray("items")) {
-								if (!itemElement.isJsonObject()) continue;
-								JsonObject itemObj = itemElement.getAsJsonObject();
+					if (object.has("items") && object.get("items").isJsonArray()) {
+						for (JsonElement itemElement : object.getAsJsonArray("items")) {
+							if (!itemElement.isJsonObject()) continue;
+							JsonObject itemObj = itemElement.getAsJsonObject();
 
-								if (itemObj.has("id") && itemObj.get("id").isJsonObject()) {
-									JsonObject idObj = itemObj.getAsJsonObject("id");
+							if (itemObj.has("id") && itemObj.get("id").isJsonObject()) {
+								JsonObject idObj = itemObj.getAsJsonObject("id");
 
-									if (idObj.has("videoId") && idObj.get("videoId").isJsonPrimitive()) {
-										videoId = idObj.getAsJsonPrimitive("videoId").getAsString();
-									}
+								if (idObj.has("videoId") && idObj.get("videoId").isJsonPrimitive()) {
+									videoId = idObj.getAsJsonPrimitive("videoId").getAsString();
 								}
-
-								if (itemObj.has("snippet") && itemObj.get("snippet").isJsonObject()) {
-									JsonObject snippetObj = itemObj.getAsJsonObject("snippet");
-
-									if (snippetObj.has("title") && snippetObj.get("title").isJsonPrimitive()) {
-										title = snippetObj.getAsJsonPrimitive("title").getAsString();
-									}
-								}
-
-								break;
 							}
-						}
 
-					} catch (UnirestException | UnsupportedEncodingException e) {
-						e.printStackTrace();
+							if (itemObj.has("snippet") && itemObj.get("snippet").isJsonObject()) {
+								JsonObject snippetObj = itemObj.getAsJsonObject("snippet");
+
+								if (snippetObj.has("title") && snippetObj.get("title").isJsonPrimitive()) {
+									title = snippetObj.getAsJsonPrimitive("title").getAsString();
+								}
+							}
+
+							break;
+						}
 					}
 
-					if (videoId == null || title == null) return;
+				} catch (UnirestException | UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
 
-					if (WizardryBot.ffmpegExe == null || WizardryBot.ffProbe == null) {
-						message.getChannel().sendMessage("Ffmpeg could not be found. Yell at my maker.");
+				if (videoId == null || title == null) return;
+
+				if (WizardryBot.ffmpegExe == null || WizardryBot.ffProbe == null) {
+					message.getChannel().sendMessage("Ffmpeg could not be found. Yell at my maker.");
+					return;
+				}
+
+				try {
+					String url = "https://www.youtube.com/watch?v=" + videoId;
+					File downloadDir = new File("downloads/");
+					if (!downloadDir.exists()) downloadDir.mkdirs();
+
+					File finishedMp3 = new File(downloadDir, title + ".mp3");
+					if (finishedMp3.exists()) {
+						message.getChannel().sendMessage(finishedMp3);
 						return;
 					}
 
-					try {
-						String url = "https://www.youtube.com/watch?v=" + videoId;
-						File downloadDir = new File("downloads/");
-						if (!downloadDir.exists()) downloadDir.mkdirs();
+					File audio = new File(downloadDir, title + ".webm");
 
-						File finishedMp3 = new File(downloadDir, title + ".mp3");
-						if (finishedMp3.exists()) {
-							message.getChannel().sendMessage(finishedMp3);
-							return;
-						}
+					if (!audio.exists()) {
 
-						File audio = new File(downloadDir, title + ".webm");
+						File bin = new File("bin/youtube-dl.exe");
+						YoutubeDL.setExecutablePath(bin.getAbsolutePath());
+						YoutubeDLRequest request = new YoutubeDLRequest(url, downloadDir.getPath());
+						request.setOption("format", "bestaudio");
 
-						if (!audio.exists()) {
+						YoutubeDLResponse response = YoutubeDL.execute(request);
+						String stdOut = response.getOut();
 
-							File bin = new File("bin/youtube-dl.exe");
-							YoutubeDL.setExecutablePath(bin.getAbsolutePath());
-							YoutubeDLRequest request = new YoutubeDLRequest(url, downloadDir.getPath());
-							request.setOption("format", "bestaudio");
-
-							YoutubeDLResponse response = YoutubeDL.execute(request);
-							String stdOut = response.getOut();
-
-							System.out.println(stdOut);
-						}
-
-						File findAudio = findFileContainingName(downloadDir, title);
-
-						if (findAudio == null) return;
-						if (!audio.getName().equals(findAudio.getName())) {
-							findAudio.renameTo(audio);
-
-							//	audio = findAudio;
-						}
-
-						FFmpeg ffmpeg = new FFmpeg(WizardryBot.ffmpegExe.getPath());
-						FFprobe ffprobe = new FFprobe(WizardryBot.ffProbe.getPath());
-						if (!audio.exists()) {
-							message.getChannel().sendMessage("Couldn't download the video");
-							return;
-						}
-
-						FFmpegProbeResult ffmpegResult = ffprobe.probe(audio.getPath());
-						ffmpegResult.format.bit_rate = 96_000;
-
-						FFmpegBuilder builder = new FFmpegBuilder()
-								.setInput(ffmpegResult)
-								.overrideOutputFiles(true)
-								.addOutput("downloads/" + title + ".mp3")
-								.setFormat("mp3")
-								.done();
-
-						FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-
-						executor.createJob(builder).run();
-
-						audio.delete();
-						File mp3 = new File(downloadDir, title + ".mp3");
-
-						if (!mp3.exists()) {
-							message.getChannel().sendMessage("Couldn't compress video");
-							return;
-						}
-
-						message.getChannel().sendMessage(mp3);
-					} catch (Exception e) {
-						throw new RuntimeException(e);
+						System.out.println(stdOut);
 					}
 
+					File findAudio = findFileContainingName(downloadDir, title);
+
+					if (findAudio == null) return;
+					if (!audio.getName().equals(findAudio.getName())) {
+						findAudio.renameTo(audio);
+
+						//	audio = findAudio;
+					}
+
+					FFmpeg ffmpeg = new FFmpeg(WizardryBot.ffmpegExe.getPath());
+					FFprobe ffprobe = new FFprobe(WizardryBot.ffProbe.getPath());
+					if (!audio.exists()) {
+						message.getChannel().sendMessage("Couldn't download the video");
+						return;
+					}
+
+					FFmpegProbeResult ffmpegResult = ffprobe.probe(audio.getPath());
+					ffmpegResult.format.bit_rate = 96_000;
+
+					FFmpegBuilder builder = new FFmpegBuilder()
+							.setInput(ffmpegResult)
+							.overrideOutputFiles(true)
+							.addOutput("downloads/" + title + ".mp3")
+							.setFormat("mp3")
+							.done();
+
+					FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+					executor.createJob(builder).run();
+
+					audio.delete();
+					File mp3 = new File(downloadDir, title + ".mp3");
+
+					if (!mp3.exists()) {
+						message.getChannel().sendMessage("Couldn't compress video");
+						return;
+					}
+
+					message.getChannel().sendMessage(mp3);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
 				}
-			});
-			thread.start();
-		}
+
+			}
+		});
+		thread.start();
 	}
 
 	@Override
