e6e043fd038e499b3f61ba1646e05846a6d022af
Demoniaque <saadodi44@gmail.com>
2018-08-14 17:44:24 +0300

    remove other debug

--- src/main/java/com/teamwizardry/wizardrybot/WizardryBot.java
+++ src/main/java/com/teamwizardry/wizardrybot/WizardryBot.java
@@ -211,7 +211,7 @@ public class WizardryBot {
 
 		api.addMessageCreateListener(messageCreateEvent -> {
 
-			if (messageCreateEvent.getChannel().getId() == 407963020631736323L)
+			//	if (messageCreateEvent.getChannel().getId() == 407963020631736323L)
 
 				processMessage(messageCreateEvent.getMessage(), messageCreateEvent.getApi());
 		});
